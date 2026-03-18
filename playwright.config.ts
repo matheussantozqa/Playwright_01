cb7518a4ebcd7cfe9ab5cbb3cd4ca40c7e6f956d
Matheus <matheussantoz.qa@gmail.com>
2026-03-18 00:38:52 -0300

    change trace config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,13 +12,15 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 5 * 1000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+//  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -30,7 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     // headless: false,
-    trace: 'on-first-retry',
+    trace: 'on-all-retries',
   },
 
   /* Configure projects for major browsers */
