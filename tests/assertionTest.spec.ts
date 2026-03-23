9da08da4cd728582eb25a18902b89be434e8550e
matheussantozqa <matheussantoz.qa@gmail.com>
2026-03-23 16:11:50 -0300

    Assertion Tests page done

--- tests/assertionTest.spec.ts
+++ tests/assertionTest.spec.ts
@@ -15,3 +15,46 @@ test('Enabled/Disabled Assertion', async ({page}) => {
     await expect(page.locator('[placeholder="Enabled/Disabled Field"]')).toBeDisabled();
     await page.close()
 });
+
+test('Text Match/Mismatch Assertion', async ({page}) => {
+    await page.goto('https://www.letskodeit.com/practice');
+    await expect(page.locator('#mousehover')).toHaveText('Mouse Hover');
+    await expect(page.locator('#mousehover')).not.toHaveText('RandomText');
+    await page.close()
+});
+
+test('Attribute Assertion', async ({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await expect(page.locator('[placeholder="Username"]')).toHaveAttribute('name', 'username');
+    await expect(page.locator('[placeholder="Username"]')).toHaveAttribute('class', /.*oxd-input./);
+    await page.close()
+});
+
+test('URL Assertion', async ({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+
+    //Full URL Assertion
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+    //Partial URL Assertion
+    await expect(page).toHaveURL(/orangehrmlive/);
+    await page.close()
+});
+
+test('Title Assertion', async ({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+
+    //Full Title Assertion
+    await expect(page).toHaveTitle('OrangeHRM');
+
+    //Partial Title Assertion
+    await expect(page).toHaveTitle(/HRM/);
+    await page.close()
+});
+
+test.only('Screenshots Assertion', async ({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await page.waitForTimeout(3000);
+    await expect(page).toHaveScreenshot('orangehrm-homepage.png');
+    await page.close()
+});
