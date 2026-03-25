4a1e418eea8069e7bee0f24444fb0832ce12a628
matheussantozqa <matheussantoz.qa@gmail.com>
2026-03-25 18:19:44 -0300

    Checkboxes Test assertion add

---
+++ tests/checkboxHandling.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('Checkbox Button Handling', async ({ page }) => {
+    await page.goto('https://demo.automationtesting.in/Register.html');
+    const cricketCheckbox = page.locator('#checkbox1');
+    const moviesCheckbox = page.locator('#checkbox2');
+    const hockeyCheckbox = page.locator('#checkbox3');
+
+    //Way 1 Assertion
+    expect(cricketCheckbox).not.toBeChecked();
+    expect(moviesCheckbox).not.toBeChecked();
+    expect(hockeyCheckbox).not.toBeChecked();
+
+    //Way 2 Assertion
+    expect(await cricketCheckbox.isChecked()).toBeFalsy();
+    expect(await moviesCheckbox.isChecked()).toBeFalsy();
+    expect(await hockeyCheckbox.isChecked()).toBeFalsy();
+
+    await cricketCheckbox.check();
+    await moviesCheckbox.check();
+    await hockeyCheckbox.check();
+
+    expect(cricketCheckbox).toBeChecked();
+    expect(moviesCheckbox).toBeChecked();
+    expect(await hockeyCheckbox.isChecked()).toBeTruthy();
+
+
+
+
+
+
+    await page.close();
+});
